dee5bffb29c6952977cb5199e76da02cdd8d1450
Tommy Rafael Sánchez Feliz <tommy-s05@hotmail.com>
2023-09-27 10:26:07 -0400

    Popular properties obtenidas desde el API.

--- src/components/Heros/HeroHome.js
+++ src/components/Heros/HeroHome.js
@@ -1,18 +1,18 @@
 import Search from "@/components/Search";
 import Image from "next/image";
 
-export default function Hero() {
+export default function HeroHome() {
     return (
         <section className="relative">
             <div className={"absolute flex-col flex justify-center items-center h-full w-full space-y-5 "}>
                 <h2 className={"font-bold text-3xl md:text-5xl lg:text-7xl text-white p-5 m-5 px-10 backdrop-blur-sm z-10"}>
                     Encuentra tu lugar ideal
                 </h2>
-                
+
                 <button className=" bg-primary rounded-full p-4 uppercase text-white  block sm:hidden z-10">
                     Inicia tu busqueda
                 </button>
-                
+
                 <Search/>
             </div>
             {/*<img*/}
