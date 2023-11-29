397146c06a711dc8591a40f2ce0a5d66c7a455ae
Tommy-S05 <tommy-s05f@hotmail.com>
2023-11-29 14:02:58 -0400

    Envio de formulario contactanos

--- src/components/Contactus/ContactusForm.js
+++ src/components/Contactus/ContactusForm.js
@@ -3,23 +3,33 @@ import {Button} from "@nextui-org/button";
 
 export default function ContactusForm() {
     return (
-        <form className="space-y-2 ">
+        <form
+            action={"https://formspree.io/f/xleyddre"}
+            method={"POST"}
+            className={"space-y-2"}
+        >
             <Input
                 label={"Nombre completo"}
+                name={'Nombre'}
                 variant={"bordered"}
                 color={'secondary'}
             />
             <Input
                 label={'Correo electrónico'}
+                name={'Correo electrónico'}
                 variant={"bordered"}
                 color={'secondary'}
             />
             <Textarea
                 label={"Escribe tu mensaje aquí"}
+                name={'Mensaje'}
                 variant={'bordered'}
                 color={'secondary'}
             />
-            <Button color="primary">
+            <Button
+                color={"primary"}
+                type={'submit'}
+            >
                 Enviar
             </Button>
         </form>
